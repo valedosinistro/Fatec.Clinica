5bc7f07c349f9ea50c6cfd1f7b8fa72e6158ffd5
Mateus <mateus.amaral@fatec.sp.gov.br>
2018-08-01 00:23:12 -0300

    Criado o script da tabela Paciente

---
+++ Fatec.Clinica.Database/dbo/Paciente.sql
@@ -0,0 +1,11 @@
+create table Paciente (
+Id int not null identity,
+Nome varchar(100) not null,
+Telefone varchar(13) not null,
+Sexo char not null,
+Data_Nasc Date not null,
+Ativo bit not null,
+IdUsuario int not null,
+Constraint [PK_Paciente] Primary Key (Id),
+Constraint [FK_Paciente_Usuario] foreign key (IdUsuario) references [Usuario]([id])
+)
