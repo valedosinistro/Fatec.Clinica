9395d18d09b4b0e877c3eedde3157f84fdafe860
Pedro Bruini <pedro.bruini@hotmail.com>
2018-08-01 00:29:57 -0300

    Criado o script Consulta
    - Add referencia do script Paciente no projeto

---
+++ Fatec.Clinica.Database/dbo/Consulta.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Consulta]
+(
+	[Id] INT NOT NULL IDENTITY,
+	[IdPaciente] INT NOT NULL,
+	[IdMedico] INT NOT NULL,
+	[DataConsulta] DATE NOT NULL,
+	[Horario] TIME NOT NULL,
+
+	CONSTRAINT [PK_Consulta] PRIMARY KEY (Id),
+	CONSTRAINT [FK_Consulta_Paciente] FOREIGN KEY ([IdPaciente]) REFERENCES [Paciente]([Id]),
+	CONSTRAINT [FK_Consulta_Medico] FOREIGN KEY ([IdMedico]) REFERENCES [Medico]([Id])
+)
