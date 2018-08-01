d3d0e6c0166143d87abc01adff31a9bd27a6bb45
Pedro Bruini <pedro.bruini@hotmail.com>
2018-07-31 23:52:33 -0300

    Alterado script Medico

--- Fatec.Clinica.Database/dbo/Medico.sql
+++ Fatec.Clinica.Database/dbo/Medico.sql
@@ -1,9 +1,18 @@
 ﻿CREATE TABLE [dbo].[Medico]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Id] INT NOT NULL IDENTITY, 
 	[IdEspecialidade] INT NOT NULL,
+	[IdUsuario] INT NOT NULL,
     [Nome] VARCHAR(100) NOT NULL, 
-    [CPF] VARCHAR(11) NOT NULL, 
-    [CRM] VARCHAR(10) NOT NULL, 
-    CONSTRAINT [FK_Medico_Especialidade] FOREIGN KEY (IdEspecialidade) REFERENCES [Especialidade]([Id])
+    [Crm] VARCHAR(10) NOT NULL, 
+	[Telefone_r] VARCHAR(13) NOT NULL,
+	[Telefone_c] VARCHAR(13) NOT NULL,
+	[Endereco_c] VARCHAR(100) NOT NULL,
+	[Cidade] VARCHAR(50) NOT NULL,
+	[Estado] VARCHAR(2) NOT NULL,
+	[Ativo] BIT NOT NULL,
+
+	CONSTRAINT [PK_Medico] PRIMARY KEY (Id),
+    CONSTRAINT [FK_Medico_Especialidade] FOREIGN KEY (IdEspecialidade) REFERENCES [Especialidade]([Id]),
+	CONSTRAINT [FK_Medico_Usuario] FOREIGN KEY (IdUsuario) REFERENCES [Usuario]([Id])
 )
